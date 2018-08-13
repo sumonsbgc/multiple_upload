1d793289e2263031df7aaa752e03a43c9420c8f8
sumonsbgc <sumonsbgc@gmail.com>
2018-08-14 05:40:07 +0600

    commint ing curls some new feature

--- download.php
+++ download.php
@@ -16,3 +16,27 @@ echo $result;
 
 
 curl_close($curl_init);
+
+
+// $search_string = "";
+// $url = "https://www.w3schools.com";
+
+// $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL, $url);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// curl_setopt($ch, CURLOPT_HEADER, false);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+
+// curl_setopt($ch, CURLOPT_SSL_VERIFYSTATUS, true);
+// curl_setopt($ch, CURLOPT_HEADER, true);
+// curl_setopt($ch, CURLOPT_HTTPGET, false);
+// curl_setopt($ch, CURLOPT_HTTPPOST, false);
+
+// curl_setopt($ch, CURLOPT_POSTFIELDS, true);
+// curl_setopt($ch, CURLOPT_SAFE_UPLOAD, true);
+// curl_setopt($ch, CURLOPT_UPLOAD, true);
+
+// $result = curl_exec($ch);
+
+// echo $result;
