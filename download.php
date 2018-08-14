fe3966457b22402ec44b7312a30d57869000bf43
sumonsbgc <sumonsbgc@gmail.com>
2018-08-14 14:12:03 +0600

    comiting download any images form any url

--- download.php
+++ download.php
@@ -1,8 +1,21 @@
 <?php
 
+$url = 'http://intaconex.com/bitcoinuni/wp-content/uploads/2018/07/900bw.png';
 $curl_init = curl_init();
 
-curl_setopt($curl_init, CURLOPT_URL, 'https://www.amazon.com');
+$dir = "upload/";
+
+// $imageName = basename($url);
+// $save_location = $dir.$imageName;
+// $fo = fopen($save_location, 'wb');
+// curl_setopt($curl_init, CURLOPT_FILE, $fo);
+// curl_setopt($curl_init, CURLOPT_HEADER, 0);
+// curl_exec($curl_init);
+
+// fclose($fo);
+
+
+curl_setopt($curl_init, CURLOPT_URL, 'https://www.pinterest.com');
 curl_setopt($curl_init, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($curl_init, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($curl_init);
